77acfc6be52a4189069bad48fe71ebbeac1fbca5
Rutujakalyane <kalyanerutuja7@gmail.com>
2020-04-28 20:17:36 +0530

    REFACTORED:One generic method to find max

--- main/java/generics/MaximumValue.java
+++ main/java/generics/MaximumValue.java
@@ -1,8 +1,44 @@
 package generics;
 
-public class MaximumValue {
+import java.util.Arrays;
 
-    public Integer maximum(Integer first, Integer second, Integer third) {
+public class MaximumValue <T extends Comparable<T>> {
+
+    private T first;
+    private T second;
+    private T third;
+
+    public MaximumValue() {
+    }
+
+    public MaximumValue(T first, T second, T third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+
+    public T maximum(T first, T second, T third) {
+        if (first.compareTo(second) > 0 && first.compareTo(third) > 0) {
+            return first;
+        } else if (second.compareTo(first) > 0 && second.compareTo(third) > 0) {
+            return second;
+        } else {
+            return third;
+        }
+    }
+
+    public <T extends Comparable<T>> T testMaximum() {
+        return (T) maximum(first, second, third);
+    }
+
+    public T maximum(T... param) {
+        Arrays.sort(param);
+        return param[param.length - 1];
+    }
+}
+
+
+    /*public Integer maximum(Integer first, Integer second, Integer third) {
         if (first.compareTo(second) > 0 && first.compareTo(third) > 0) {
             return first;
         } else if (second.compareTo(first) > 0 && second.compareTo(third) > 0) {
@@ -28,6 +64,6 @@ public class MaximumValue {
         } else {
             return third;
         }
-    }
+    }*/
 
-}
+
--- test/java/generics/MaximumTest.java
+++ test/java/generics/MaximumTest.java
@@ -4,50 +4,45 @@ import org.junit.Test;
 
 public class MaximumTest {
     MaximumValue maximumValue = new MaximumValue();
+
     @Test
-    public void findingMaxValue_FromGiven_ThreeIntegers_AtFirstPosition() {
-        Integer maxValue = maximumValue.maximum(8, 5, 3);
+    public void findingMaxValue_FromGiven_ThreeIntegers_ByGeneric() {
+        Integer maxValue = (Integer) maximumValue.maximum(8, 5, 3);
         Assert.assertEquals((Integer) 8, maxValue);
     }
+
     @Test
-    public void findingMaxValue_FromGiven_ThreeIntegers_AtSecondPosition() {
-        Integer maxValue = maximumValue.maximum(5,8,3);
-        Assert.assertEquals((Integer) 8, maxValue);
-    }
-    @Test
-    public void findingMaxValue_FromGiven_ThreeIntegers_AtThirdPosition() {
-        Integer maxValue = maximumValue.maximum(3,5,8);
-        Assert.assertEquals((Integer) 8, maxValue);
-    }
-    @Test
-    public void findingMaxValue_FromGiven_ThreeFloats_AtFirstPosition() {
-        Float maxValue = maximumValue.maximum(5.5f,4.4f,8.8f);
+    public void findingMaxValue_FromGiven_ThreeFloats() {
+        Float maxValue = (Float) maximumValue.maximum(5.5f,4.4f,8.8f);
         Assert.assertEquals((Float) 8.8f, maxValue);
     }
+
     @Test
-    public void findingMaxValue_FromGiven_ThreeFloats_AtSecondPosition() {
-        Float maxValue = maximumValue.maximum(3.5f,8.8f,4.5f);
-        Assert.assertEquals((Float) 8.8f, maxValue);
-    }
-    @Test
-    public void findingMaxValue_FromGiven_ThreeFloats_AtThirdPosition() {
-        Float maxValue = maximumValue.maximum(5.4f,2.8f,6.9f);
-        Assert.assertEquals((Float) 6.9f, maxValue);
-    }
-    @Test
-    public void findingMaxValue_FromGiven_ThreeStrings_AtFirstPosition() {
+    public void findingMaxValue_FromGiven_ThreeStrings() {
         String maxValue = (String) maximumValue.maximum("Peach","Apple","Banana");
         Assert.assertEquals("Peach", maxValue);
     }
     @Test
-    public void findingMaxValue_FromGiven_ThreeStrings_AtSecondPosition() {
-        String maxValue = (String) maximumValue.maximum("Apple","Peach","Banana");
-        Assert.assertEquals("Peach", maxValue);
-    }
-    @Test
-    public void findingMaxValue_FromGiven_ThreeStrings_AtThirdPosition() {
-        String maxValue = (String) maximumValue.maximum("Apple","Banana","Peach");
-        Assert.assertEquals("Peach", maxValue);
+    public void findingMaxValue_FromGiven_IntegerElements_ByGenericClass() {
+        MaximumValue maximumValue = new MaximumValue();
+        Object maxValue = (Object) maximumValue.maximum(3, 44, 66, 2, 11, 4, 66, 7, 8, 9, 100, 46, 113);
+        Assert.assertEquals((Object) 113, maxValue);
     }
 
+    @Test
+    public void findingMaxValue_FromGiven_FloatElements_ByGenericClass() {
+        MaximumValue maximumValue = new MaximumValue();
+        Object maxValue = (Object) maximumValue.maximum(3.44f, 44.4f, 66.34f, 2.77f, 11.333f);
+        Assert.assertEquals((Object) 66.34f, maxValue);
+    }
+
+    @Test
+    public void findingMaxValue_FromGiven_StringElements_ByGenericClass() {
+        MaximumValue maximumValue = new MaximumValue();
+        Object maxValue = (Object) maximumValue.maximum("Peach", "Banana","Apple","Kiwi");
+        Assert.assertEquals((Object) "Peach", maxValue);
+    }
+
+
 }
+
