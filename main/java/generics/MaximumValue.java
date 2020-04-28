c67818f2a540cdecb1525d2515817e81d4d235e0
Rutujakalyane <kalyanerutuja7@gmail.com>
2020-04-28 17:19:24 +0530

    ADDED:[TC 2.1]Max float at first position

--- main/java/generics/MaximumValue.java
+++ main/java/generics/MaximumValue.java
@@ -10,4 +10,13 @@ public class MaximumValue {
             return third;
         }
     }
+    public Float maximum(Float first, Float second, Float third) {
+        if (first.compareTo(second) > 0 && first.compareTo(third) > 0) {
+            return first;
+        } else if (second.compareTo(first) > 0 && second.compareTo(third) > 0) {
+            return second;
+        } else {
+            return third;
+        }
+    }
 }
--- test/java/generics/MaximumTest.java
+++ test/java/generics/MaximumTest.java
@@ -21,4 +21,12 @@ public class MaximumTest {
         Integer maxValue = maximumValue.maximum(3,5,8);
         Assert.assertEquals((Integer) 8, maxValue);
     }
+    @Test
+    public void findingMaxValue_FromGiven_ThreeFloats_AtFirstPosition() {
+        MaximumValue maximumValue = new MaximumValue();
+        Float maxValue = maximumValue.maximum(5.5f,4.4f,8.8f);
+        Assert.assertEquals((Float) 8.8f, maxValue);
+    }
+
+   
 }
