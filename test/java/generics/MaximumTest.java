f589f5f5269faa226fdeafae275908ba4bb72921
Rutujakalyane <kalyanerutuja7@gmail.com>
2020-04-28 16:44:15 +0530

    ADDED:[TC 1.2]Max integer at second position

--- test/java/generics/MaximumTest.java
+++ test/java/generics/MaximumTest.java
@@ -6,7 +6,13 @@ public class MaximumTest {
     @Test
     public void findingMaxValue_FromGiven_ThreeIntegers_AtFirstPosition() {
         MaximumValue maximumValue = new MaximumValue();
-        Integer maxValue = maximumValue.maximum(5, 8, 3);
-        Assert.assertEquals((Integer) 5, maxValue);
+        Integer maxValue = maximumValue.maximum(8, 5, 3);
+        Assert.assertEquals((Integer) 8, maxValue);
+    }
+    @Test
+    public void findingMaxValue_FromGiven_ThreeIntegers_AtSecondPosition() {
+        MaximumValue maximumValue = new MaximumValue();
+        Integer maxValue = maximumValue.maximum(5,8,3);
+        Assert.assertEquals((Integer) 8, maxValue);
     }
 }
